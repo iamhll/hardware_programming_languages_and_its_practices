320aa8b4a5f7428337ac9542ae719b2e51ab3285
llhuang <15221266039@139.com>
2025-03-18 12:29:03 +0800

    5th lecture

---
+++ source/理论课/硬件编程语言与实践 - 第04讲 - 组合逻辑设计/source/test_double.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <math.h>
+
+double randomDouble(double min, double max) {
+    double random = ((double)rand()) / RAND_MAX;
+    return min + random * (max - min);
+}
+
+int main() {
+    srand(time(NULL));
+    double min = 0;
+    double max = 1000000;
+    long   bit;
+    double num = randomDouble(min, max);
+    memcpy(&bit, &num, sizeof(double));
+    long   sign     = bit >> 63;
+    long   exponent = (bit >> 52) & 0x7ff;
+    long   mantissa = bit & 0xfffffffffffff;
+    double numCalc = (1 + (double)mantissa / pow(2, 52)) * pow(2, (double)exponent - 1023);
+    printf("random number                   : %f\n", num);
+    printf("bit                             : %lx\n", bit);
+    printf("sign                            : %lx\n", sign);
+    printf("mantissa                        : %lx\n", mantissa);
+    printf("exponent                        : %lx\n", exponent);
+    printf("(-1)^s * (1+M/2^52) * 2^(e-1023): %f\n", numCalc);
+    if (num != numCalc)
+        printf("ERROR\n!");
+    return 0;
+}
---
+++ source/理论课/硬件编程语言与实践 - 第04讲 - 组合逻辑设计/source/test_float.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <math.h>
+
+float randomFloat(float min, float max) {
+    float random = ((float)rand()) / RAND_MAX;
+    return min + random * (max - min);
+}
+
+int main() {
+    srand(time(NULL));
+    float min = 0;
+    float max = 1000000;
+    int bit;
+    float num = randomFloat(min, max);
+    memcpy(&bit, &num, sizeof(float));
+    int    sign     = bit >> 31;
+    int    exponent = (bit >> 23) & 0xff;
+    int    mantissa = bit & 0x007fffff;
+    double numCalc = (1 + (double)mantissa / pow(2, 23)) * pow(2, (double)exponent - 127);
+    printf("random number                  : %f\n", num);
+    printf("bit                            : %x\n", bit);
+    printf("sign                           : %x\n", sign);
+    printf("mantissa                       : %x\n", mantissa);
+    printf("exponent                       : %x\n", exponent);
+    printf("(-1)^s * (1+M/2^23) * 2^(e-127): %f\n", numCalc);
+    if (num != numCalc)
+        printf("ERROR\n!");
+    return 0;
+}
